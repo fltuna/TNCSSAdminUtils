7379cb9bae710f55dd2cebe42898b05110a88923
faketuna <faketuna.st@gmail.com>
2025-08-25 18:34:59 +0900

    modify: rename convar

--- TNCSSAdminUtils/SimpleDebugLogger.cs
+++ TNCSSAdminUtils/SimpleDebugLogger.cs
@@ -9,13 +9,13 @@ namespace TNCSSAdminUtils;
 
 public sealed class SimpleDebugLogger : AbstractDebugLoggerBase
 {
-    public readonly FakeConVar<int> DebugLogLevelConVar = new("____TNCSS_PLUGIN_TEMPLATE_DEBUG_LOG_LEVEL",
+    public readonly FakeConVar<int> DebugLogLevelConVar = new("tncssau_debug_log_level",
         "0: Nothing, 1: Print info, warn, error message, 2: Print previous one and debug message, 3: Print previous one and trace message", 0, ConVarFlags.FCVAR_NONE,
         new RangeValidator<int>(0, 3));
     
-    public readonly FakeConVar<bool> PrintToAdminClientsConsoleConVar = new("____TNCSS_PLUGIN_TEMPLATE_DEBUG_SHOW_CONSOLE", "Debug message shown in client console?", false);
+    public readonly FakeConVar<bool> PrintToAdminClientsConsoleConVar = new("tncssau_debug_show_console", "Debug message shown in client console?", false);
     
-    public readonly FakeConVar<string> RequiredFlagForPrintToConsoleConVar = new ("____TNCSS_PLUGIN_TEMPLATE_DEBUG_CONSOLE_PRINT_REQUIRED_FLAG", "Required flag for print to client console", "css/generic");
+    public readonly FakeConVar<string> RequiredFlagForPrintToConsoleConVar = new ("tncssau_debug_console_print_required_flag", "Required flag for print to client console", "css/generic");
 
     
     public override int DebugLogLevel => DebugLogLevelConVar.Value;
